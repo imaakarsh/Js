aaf91132d2284b76a6967e4cb9d3a9636c4bc84f
imaakarsh <aakarsh027@outlook.com>
2026-03-02 20:26:12 +0530

    feat: add array manipulation examples in 07-array.js

---
+++ 07-array.js
@@ -0,0 +1,12 @@
+const carriage1 = ["Veer","Ayush","Ravi"]
+const emptyCarriage = []
+
+const threeEmptySeats = Array(3)
+console.log(threeEmptySeats.length)
+const passengers = Array('Veer',"Ayush","Ravi")
+
+const singlePassenger = Array.of(3)
+console.log(singlePassenger)
+
+const trainCode = Array.from("Dust")
+console.log(trainCode)
